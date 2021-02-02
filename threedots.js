5b92651e43a0014cb0a0028b9c93a643893d79d8
jamir ali <jamirali720@gmail.com>
2021-02-02 19:24:02 +0600

    added

---
+++ threedots.js
@@ -0,0 +1,16 @@
+// const ages = [13, 33, 45, 25, 26, 46, 36, 60, 16];
+// const ages2 = [23, 42, 15,  35, 16, 26, 16];
+// const ages3 = [21, 32,31, 36, 73, 18, 70];
+// const max = Math.max(...ages3)
+// console.log(max)
+// const allages = ages.concat(ages2).concat(ages3);
+// // console.log(allages)
+// allages2 = [...ages, ...ages2, ...ages3];
+// console.log(allages2)
+const business = 680;
+const minister = 360;
+const sochib = 606;
+// const bigger = Math.max(business, minister, sochib);
+const biggerMan = [680, 360, 606];
+const max = Math.max(...biggerMan)
+console.log(max)
